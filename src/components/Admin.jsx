f4a90193dc9a4a18ead11a393499c139ebb96023
Hailey Gaylor <haileygaylor@gmail.com>
2019-12-01 22:45:37 -0800

    completed page for admin

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import OrderList from './OrderList';
 
-function Admin() {
+function Admin(props) {
     return (
         <div>
             <h2>Admin</h2>
+            <OrderList
+                orderList={props.orderList}
+                currentRouterPath={props.currentRouterPath} />
         </div>
     );
 }
+
+Admin.propTypes = {
+    orderList: PropTypes.array,
+    currentRouterPath: PropTypes.string.isRequired
+};
 
 export default Admin;
