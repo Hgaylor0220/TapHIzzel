9f2af255b6bfee478a51d72a29339133aafb0481
Hailey Gaylor <haileygaylor@gmail.com>
2019-12-01 22:36:19 -0800

    completed order.jsx

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -2,19 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Order(props) {
-    return (
+    const orderInformation =
         <div>
             <h3>{props.type} - {props.name}</h3>
-            <p><em>{props.brand}</em></p>
+            <h4>{props.brand}</h4>
             <hr />
-        </div>
-    );
+        </div>;
+    if (props.currentRouterPath === '/admin') {
+        return (
+            <div onClick={() => { alert('View the order for' + props.name); }}>
+                {orderInformation}
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                {orderInformation}
+            </div>
+        );
+    }
 }
-
 Order.propTypes = {
     name: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
-    brand: PropTypes.string
+    brand: PropTypes.string,
+    currentRouterPath: PropTypes.string
 };
 
 export default Order;
