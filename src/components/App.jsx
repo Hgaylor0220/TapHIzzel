da6571d277df358d55c47a4f0a144067b68bd359
Hailey Gaylor <haileygaylor@gmail.com>
2019-12-01 22:50:03 -0800

    refactored header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,51 +1,61 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
 import Header from './Header';
-import Home from './Home';
-import StockList from './StockList';
 import OrderList from './OrderList';
-import Admin from './Admin';
+import NewOrderControl from './NewOrderControl';
 import Error404 from './Error404';
-
-
-
+import { Switch, Route } from 'react-router-dom';
+import Admin from './Admin';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-    //         masterOrderList: []
-    //         // masterStockList: []
-        };
-    //     this.handleAddingNewOrderToList = this.handleAddingNewOrderToList.bind(this);
-    //     // this.handleAddingNewStockToList = this.handleAddingNewStockToList.bind(this);
-    
-    // NewOrderList(newOrder) {
-    //     var newMasterOrderList = this.state.masterOrderList
-    //     newmasterOrderList.push(newOrder);
-    //     this.setState({ masterOrderList: newMasterOrderList });
-    }
-    
-    render(){
-        return (
-            <div>
-                <Header/>
-                    <Switch>
-                    <Route exact path='/' component={Home}/>
-                    {/* <Route path='/stockList' render={() => <StockList stockList={this.state.masterStockList} />} />
 
-                    <Route path='/Order' render={() => <OrderList orderList={this.state.masterOrderList} />} />
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterOrderList: []
+    };
+    this.handleAddingNewOrderToList = this.handleAddingNewOrderToList.bind(this);
+  }
 
-                    <Route path='/newOrder' render={() => <NewOrderController newOrderSubmission={this.handleAddingNewOrderToList} />} />
+  componentDidMount() {
+    this.waitTimeUpdateTimer = setInterval(() =>
+      this.updateOrderElapsedWaitTime(),
+    5000
+    );
+  }
 
-                     <Route path='/admin' render={(props) => <Admin orderList={this.state.masterOrderList} currentRouterPath={props.type.pathname} />} /> */}
-                
-                     {/* <Route component={Error404} /> */}
-                </Switch>
-             </div>
+  componentWillUnmount(){
+    clearInterval(this.waitTimeUpdateTimer);
+  }
 
-             );
-         }
+  updateOrderElapsedWaitTime() {
+    let newMasterOrderList = this.state.masterOrderList.slice();
+    newMasterOrderList.forEach((ticket) =>
+      ticket.formattedWaitTime = (ticket.timeOpen).fromNow(true)
+    );
+    this.setState({masterOrderList: newMasterOrderList});
+  }
+
+  handleAddingNewOrderToList(newOrder){
+    var newMasterOrderList = this.state.masterOrderList.slice();
+    newOrder.formattedWaitTime = (newOrder.timeOpen).fromNow(true);
+    newMasterOrderList.push(newOrder);
+    this.setState({masterOrderList: newMasterOrderList});
+  }
+
+  render(){
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' render={()=><OrderList ticketList={this.state.masterOrderList} />} />
+          <Route path='/newticket' render={()=><NewOrderControl onNewOrderCreation={this.handleAddingNewOrderToList} />} />
+          <Route path='/admin' render={(props)=><Admin ticketList={this.state.masterOrderList} currentRouterPath={props.location.pathname}/>} />
+          <Route component={Error404} />
+        </Switch>
+      </div>
+    );
+  }
+
 }
 
 export default App;
